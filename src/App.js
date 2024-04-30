003d70537af20f8787243ceebe8d528bd6e2c3f8
divey26 <divendraparamendran226@gmail.com>
2024-04-30 19:40:50 +0530

    create age calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const[datebirth,setDatebirth]=useState('');
+const [age, setAge] = useState(null);
+
+
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  var age=calculateAge(datebirth)
+  setAge(age)
+
+}
+
+  const calculateAge=(birthDate)=> {
+  var birthDateObj = new Date(birthDate);
+  var currentDate = new Date();
+  var differenceMs = currentDate - birthDateObj;
+  var ageDate = new Date(differenceMs); 
+  var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  
+  return age;
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+                    <h2>Age Calculator</h2>
+                    <div className='mb-2'>
+                        <label htmlFor="dateOfBirthInput">Enter Your Date of Birth</label>
+                        <br>
+                        </br>
+                        <input type="date" id="dateInput"className='form-control-new'
+                            value={datebirth} onChange={(e) => setDatebirth(e.target.value)} />
+                    </div>
+
+                   
+                    <button className='btnw btn1' type="submit">Submit</button>                
+                </form>
+                <div className='background2'>
+                    <h3>Your age is: {age} years old</h3>
+                </div>
     </div>
   );
 }
